eade40ba2cc64135d37b3d59d9790a2cef14249c
Prashant kumar <prashantjatav563@gmail.com>
2026-02-24 22:54:55 +0530

    Restore main function with voting eligibility check

--- Dart/2.dart
+++ Dart/2.dart
@@ -1,28 +1,24 @@
 
 void main() {
 
-//     int age = int.parse(stdin.readLineSync()!);
+    int age = int.parse(stdin.readLineSync()!);
 
-//     if (age >= 18) {
-//         print("Eligible for voting");
-//     }
-//     else {
-//         print("Not eligible for voting");
-//     }
+    if (age >= 18) {
+        print("Eligible for voting");
+    }
+    else {
+        print("Not eligible for voting");
+    }
 
-//     for(int i=0; i<=5; i++){
-//         print(i);
-//     }
+    for(int i=0; i<=5; i++){
+        print(i);
+    }
 
-//     List<String> names = ["rajat", "prince", "prashant"];
-//     print(names.length);
-//     for(int i = 0; i<names.length; i++){
-//          print(names[i]);
-//     }
-
-    // create an electricity consumption program on the basic of followinng criteria:
-    // units range | 0-100, 101-200, 201-300, above 300
-    // rate per units(in rs) | 1.5, 2.5, 4.5, 5.0
+    List<String> names = ["rajat", "prince", "prashant"];
+    print(names.length);
+    for(int i = 0; i<names.length; i++){
+         print(names[i]);
+    }
     
-
 }
+
